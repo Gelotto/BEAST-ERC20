468fe7c85ed44133b9552403bfd641e024704005
Rizbe <Rizbe@users.noreply.github.com>
2024-01-27 17:55:31 -0500

    Create beast.sol

---
+++ beast.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact admin@gelotto.io 
+contract GelottoBEAST is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner)
+        ERC20("Gelotto BEAST", "BEAST")
+        Ownable(initialOwner)
+        ERC20Permit("Gelotto BEAST")
+    {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    // Override the decimals function to set a new decimal place
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
